0e27ffc2ad94038f60143373d960eb0d5aaf174c
Pedro Furlan <pedrofurlan6@gmail.com>
2023-01-13 18:44:17 -0300

    Deleting directory 'shirts' and update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Image from 'next/image'
 import { HomeContainer, Product } from '../styles/pages/home'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
-import camiseta1 from '../images/shirts/1.png'
-import camiseta2 from '../images/shirts/2.png'
-import camiseta3 from '../images/shirts/3.png'
 import { stripe } from '../lib/stripe'
 import Stripe from 'stripe'
 
@@ -46,7 +43,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   })
@@ -66,5 +63,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2,
   }
 }
